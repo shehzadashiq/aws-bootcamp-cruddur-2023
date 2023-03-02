b5e3e2e734ed2ab974486f0ff7c717158f7c2a97
shehzadashiq <shehzadashiq@hotmail.com>
2023-03-02 18:52:18 +0000

    Remove x-ray config

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -45,9 +45,9 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 # X-Ray
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+# xray_url = os.getenv("AWS_XRAY_URL")
+# xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+# XRayMiddleware(app, xray_recorder)
 
 app = Flask(__name__)
 
