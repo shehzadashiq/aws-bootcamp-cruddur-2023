628913aa49408cc544e5cd6f138f10303ebf48a7
Shehzad <shehzadashiq@hotmail.com>
2023-03-16 21:48:30 +0000

    Adding RDS Support
    Signed-off-by: Shehzad <shehzadashiq@hotmail.com>

---
+++ readEnv.ps1
@@ -0,0 +1,7 @@
+Get-Content .env | foreach {
+  $name, $value = $_.split('=')
+  if ([string]::IsNullOrWhiteSpace($name) || $name.Contains('#')) {
+   continue
+  }
+  Set-Content env:\$name $value
+}
