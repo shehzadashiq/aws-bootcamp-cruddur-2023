665ef9dd94f69b7a4988340fcd74468e7b050378
Shehzad <shehzadashiq@hotmail.com>
2023-03-18 08:17:57 +0000

    Adding lambda requirements

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -1,5 +1,9 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+-- forcefully drop our tables if they already exist
+DROP TABLE IF EXISTS public.users cascade;
+DROP TABLE IF EXISTS public.activities;
+
 CREATE TABLE public.users (
   uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
   display_name text,
