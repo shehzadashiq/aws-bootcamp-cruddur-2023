b9dd25d62153173bd56b86c0143658840c641092
shehzadashiq <shehzadashiq@hotmail.com>
2023-04-09 01:36:39 +0000

    Implement TimeZone Fixes

--- backend-flask/lib/ddb.py
+++ backend-flask/lib/ddb.py
@@ -85,7 +85,8 @@ class Ddb:
   # creates message_group and message
   def create_message(client,message_group_uuid, message, my_user_uuid, my_user_display_name, my_user_handle):
     now = datetime.now(timezone.utc).astimezone().isoformat()
-    created_at = now
+    # created_at = now
+    created_at = datetime.now().isoformat()
     message_uuid = str(uuid.uuid4())
 
     record = {
--- frontend-react-js/src/components/MessageItem.js
+++ frontend-react-js/src/components/MessageItem.js
@@ -1,24 +1,9 @@
 import './MessageItem.css';
 import { Link } from "react-router-dom";
-import { DateTime } from 'luxon';
+import { format_datetime, message_time_ago } from '../lib/DateTimeFormats';
 
 export default function MessageItem(props) {
 
-  const format_time_created_at = (value) => {
-    // format: 2050-11-20 18:32:47 +0000
-    const created = DateTime.fromISO(value)
-    const now     = DateTime.now()
-    const diff_mins = now.diff(created, 'minutes').toObject().minutes;
-    const diff_hours = now.diff(created, 'hours').toObject().hours;
-
-    if (diff_hours > 24.0){
-      return created.toFormat("LLL L");
-    } else if (diff_hours < 24.0 && diff_hours > 1.0) {
-      return `${Math.floor(diff_hours)}h`;
-    } else if (diff_hours < 1.0) {
-      return `${Math.round(diff_mins)}m`;
-    }
-  };
 
   return (
     <Link className='message_item' to={`/messages/@`+props.message.handle}>
@@ -31,8 +16,8 @@ export default function MessageItem(props) {
           </div>{/* activity_identity */}
         </div>{/* message_meta */}
         <div className="message">{props.message.message}</div>
-        <div className="created_at" title={props.message.created_at}>
-          <span className='ago'>{format_time_created_at(props.message.created_at)}</span> 
+        <div className="created_at" title={format_datetime(props.message_group.created_at)}>
+          <span className='ago'>{message_time_ago(props.message_group.created_at)}</span>
         </div>{/* created_at */}
       </div>{/* message_content */}
     </Link>
