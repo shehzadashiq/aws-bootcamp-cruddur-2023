7a11ea6077fb2dfb075969ab7c189b2ac2b16c65
Shehzad <shehzadashiq@hotmail.com>
2023-05-01 19:09:37 +0100

    Remove extensions from scripts unless custom

--- bin/cfn/deploy.sh
+++ bin/cfn/deploy.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
 set -e
 
-# CFN_PATH="/workspace/aws-bootcamp-cruddur-2023/aws/cfn/template.yaml"
 CFN_PATH="/workspace/aws-bootcamp-cruddur-2023/aws/cfn/template.yaml"
 
 cfn-lint $CFN_PATH
