cfd34490eef2a43b79f58ec13e233f0a9338eba6
shehzadashiq <shehzadashiq@hotmail.com>
2023-06-13 21:47:19 +0000

    Update CodeBuild.json and ActivityForm with accessToken

--- frontend-react-js/src/components/ActivityForm.js
+++ frontend-react-js/src/components/ActivityForm.js
@@ -2,6 +2,7 @@ import './ActivityForm.css';
 import React from "react";
 import process from 'process';
 import {ReactComponent as BombIcon} from './svg/bomb.svg';
+import { checkAuth, getAccessToken } from '../lib/CheckAuth';
 
 export default function ActivityForm(props) {
   const [count, setCount] = React.useState(0);
@@ -20,9 +21,15 @@ export default function ActivityForm(props) {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities`
       console.log('onsubmit payload', message)
       console.log("User Handle From ActivityForm.js -> ",props.user_handle.handle)
+
+      await getAccessToken()
+      const access_token = localStorage.getItem("access_token")
+      console.log('activityform', access_token)
+
       const res = await fetch(backend_url, {
         method: "POST",
         headers: {
+          'Authorization': `Bearer ${access_token}`,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
